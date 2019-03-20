199593868cd22097907d4c66c58b15cbd1da9a02
Henrik Gemal <henrik@gemal.dk>
2019-03-20 23:24:34 +0100

    fixes

--- index.js
+++ index.js
@@ -1,46 +1,62 @@
 #!/usr/bin/env node
 
 'use strict';
-
 const ExifImage = require('exif').ExifImage;
-
-var program = require('commander');
+const program = require('commander');
+/**
+ * Check a folder.
+ * @return {boolean}
+ */
+function checkFiles() {
+    var hasexif = false;
+    files.forEach(function(filePath) {
+        console.log("Checking1:" + filePath);
+        try {
+            new ExifImage({image: filePath}, function(error, exifData) {
+                console.log("Checking:" + filePath);
+                if (error) {
+                    return console.error(`Error: ${error.message}`);
+                }
+                for (var key in exifData) {
+                    if (exifData.hasOwnProperty(key)) {
+                        if (Object.keys(exifData[key]).length != 0) {
+                            console.error(exifData);
+                            console.log("set true");
+                            hasexif = true;
+                            break;
+                        }
+                    }
+                }
+            });
+        } catch (error) {
+            console.error(`Error: ${error.message}`);
+        }
+    });
+    console.log("hasexif:" + hasexif);
+    return hasexif;
+}
 
 program
-  .version(require('./package.json').version)
-  .usage('<file ...> [options]')
-  .parse(process.argv);
+        .version(require('./package.json').version)
+        .description('Checks if all images in CSS files exists')
+        .usage('<file ...> [options]')
+        .option('-v, --verbose', 'Add more output')
+        .parse(process.argv);
 
-var files = program.args;
+const files = program.args;
+console.log(files);
 
-console.info('Number of files to check: ' + files.length);
-
-var hasexif = false;
-
-files.forEach(function(filePath) {
-  try {
-    new ExifImage({ image : filePath }, function (error, exifData) {
-      console.log("Checking:" + filePath);
-      if (error) {
-        return console.log(`Error: ${error.message}`);
-      }
-      for (var key in exifData) {
-        if (exifData.hasOwnProperty(key)) {
-          if (Object.keys(exifData[key]).length != 0) {
-            console.error(exifData);
-            hasexif = true;
-            break;
-          }
-        }
-      }
-    });
-  } catch (error) {
-    console.log(`Error: ${error.message}`);
-  }
-});
-
-process.on('exit', function() {
-  if (hasexif) {
-    process.exit(1);
-  }
-});
+if (files.length > 0) {
+    console.info('Number of files to check: ' + files.length);
+    const exif = checkFiles();
+    console.log("returned");
+    console.log(exif ? "true" : "false");
+    if (exif) {
+        process.exitCode = 1;
+    } else {
+        process.exitCode = 0;
+    }
+} else {
+    console.error('Oops! No files specified');
+    process.exitCode = 2;
+}
---
+++ test/index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const assert = require('assert');
+const path = require('path');
+const spawn = require('child_process').spawn;
+const expect = require('chai').expect;
+
+describe('index.js', function() {
+    // eslint-disable-next-line no-invalid-this
+    this.timeout(8000);
+
+    it('should exit 1 having exif problems', function(done) {
+        let out = '';
+        spawn('node', [path.join(__dirname, '../index.js'), 'test/img1/img.jpg'], {
+            cwd: path.join(__dirname, '../'),
+        }).on('exit', function(code) {
+            assert.equal(code, 1);
+            expect(out).to.match(/Error found in:.*?style\.css/);
+            expect(out).to.match(/Full path not found.*?\.\.\/img\/404\.png/);
+            expect(out).to.match(/Path in CSS file: \.\.\/img\/404\.png\?v=5/);
+            expect(out).to.match(/Original path in CSS file: \.\.\/img\/404\.png/);
+            done();
+        }).stdout.on('data', function(data) {
+            console.log(data.toString());
+            out += data;
+        });
+    });
+
+});
