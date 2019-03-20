8445b6cf873e120e36b43a261fd7ce9630aa9c73
Henrik Gemal <henrik@gemal.dk>
2019-03-20 23:37:14 +0100

    fix

--- test/index.js
+++ test/index.js
@@ -11,11 +11,26 @@ describe('index.js', function() {
     // eslint-disable-next-line no-invalid-this
     this.timeout(8000);
 
+    it('should exit 0 having no exif problems', function(done) {
+        let out = '';
+        spawn('node', [path.join(__dirname, '../index.js'), 'test/img2/firefox.jpg'], {
+            cwd: path.join(__dirname, '../'),
+        }).on('exit', function(code) {
+            assert.equal(code, 0);
+            expect(out).to.match(/Number of files to check: 1/);
+            done();
+        }).stdout.on('data', function(data) {
+            console.log(data.toString());
+            out += data;
+        });
+    });
+
     it('should exit 1 having exif problems', function(done) {
         let out = '';
         spawn('node', [path.join(__dirname, '../index.js'), 'test/img1/img.jpg'], {
             cwd: path.join(__dirname, '../'),
         }).on('exit', function(code) {
+            console.log(out.toString());
             assert.equal(code, 1);
             expect(out).to.match(/Error found in:.*?style\.css/);
             expect(out).to.match(/Full path not found.*?\.\.\/img\/404\.png/);
