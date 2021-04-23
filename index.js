e3183a39a128cb5a25a260b5dec2ef0da6682567
Henrik Gemal <henrik@gemal.dk>
2021-04-23 11:26:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,27 +4,27 @@
 
 const ExifImage = require('exif').ExifImage;
 
-var program = require('commander');
+const program = require('commander');
 
 program
     .version(require('./package.json').version)
     .usage('<file ...> [options]')
     .parse(process.argv);
 
-var files = program.args;
+const files = program.args;
 
 console.info('Number of files to check: ' + files.length);
 
-var hasexif = false;
+let hasexif = false;
 
 files.forEach(function(filePath) {
     try {
-        new ExifImage({image : filePath}, function (error, exifData) {
+        new ExifImage({image: filePath}, function (error, exifData) {
             console.log('Checking: ' + filePath);
             if (error) {
                 return console.log(`Error: ${error.message}`);
             }
-            for (let key in exifData) {
+            for (const key in exifData) {
                 if (exifData.hasOwnProperty(key)) {
                     if (Object.keys(exifData[key]).length != 0) {
                         console.error(exifData);
