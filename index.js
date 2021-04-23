79bf7930a10a98c5d63849605b2ccfcf49bf7db1
Henrik Gemal <henrik@gemal.dk>
2021-04-23 11:23:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ const ExifImage = require('exif').ExifImage;
 var program = require('commander');
 
 program
-  .version(require('./package.json').version)
-  .usage('<file ...> [options]')
-  .parse(process.argv);
+    .version(require('./package.json').version)
+    .usage('<file ...> [options]')
+    .parse(process.argv);
 
 var files = program.args;
 
@@ -18,29 +18,29 @@ console.info('Number of files to check: ' + files.length);
 var hasexif = false;
 
 files.forEach(function(filePath) {
-  try {
-    new ExifImage({ image : filePath }, function (error, exifData) {
-      console.log("Checking: " + filePath);
-      if (error) {
-        return console.log(`Error: ${error.message}`);
-      }
-      for (var key in exifData) {
-        if (exifData.hasOwnProperty(key)) {
-          if (Object.keys(exifData[key]).length != 0) {
-            console.error(exifData);
-            hasexif = true;
-            break;
-          }
-        }
-      }
-    });
-  } catch (error) {
-    console.log(`Error: ${error.message}`);
-  }
+    try {
+        new ExifImage({image : filePath}, function (error, exifData) {
+            console.log('Checking: ' + filePath);
+            if (error) {
+                return console.log(`Error: ${error.message}`);
+            }
+            for (let key in exifData) {
+                if (exifData.hasOwnProperty(key)) {
+                    if (Object.keys(exifData[key]).length != 0) {
+                        console.error(exifData);
+                        hasexif = true;
+                        break;
+                    }
+                }
+            }
+        });
+    } catch (error) {
+        console.log(`Error: ${error.message}`);
+    }
 });
 
 process.on('exit', function() {
-  if (hasexif) {
-    process.exit(1);
-  }
+    if (hasexif) {
+        process.exit(1);
+    }
 });
