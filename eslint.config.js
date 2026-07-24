3cbc842efb0e02c9d023c16b66d3887434fc03fe
Henrik Gemal <henrik@gemal.dk>
2026-07-25 00:26:11 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,5 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import js from '@eslint/js';
+import globals from 'globals';
 
 export default [
     js.configs.recommended,
@@ -36,4 +36,4 @@ export default [
             'indent': ['error', 4],
         },
     },
-]
+];
